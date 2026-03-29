34cc1415e74959eae7799f1fad41ad73dfc90b43
siava79 <82262876+siava79@users.noreply.github.com>
2026-03-29 18:17:55 +0300

    Add files via upload

---
+++ autopark.py
@@ -0,0 +1,96 @@
+cars = [
+    {"brand": "BMW", "model": "X5", "year": 2005, "max_speed": 240, "price": 1368515},
+    {"brand": "BMW", "model": "X3", "year": 2017, "max_speed": 260, "price": 1798913},
+    {"brand": "Mersedes", "model": "A-Class", "year": 2022, "max_speed": 300, "price": 1798913},
+    {"brand": "Hyuinday", "model": "Accent", "year": 2017, "max_speed": 240, "price": 1698913},
+    {"brand": "Renault", "model": "Logan", "year": 2010, "max_speed": 210, "price": 120123},
+    {"brand": "Nissan", "model": "X-Trail", "year": 2021, "max_speed": 260, "price": 2698910},
+    {"brand": "Toyota", "model": "Camry", "year": 2010, "max_speed": 270, "price": 210000},
+    {"brand": "Toyota", "model": "Avalon", "year": 2013, "max_speed": 300, "price": 396125},
+]
+
+
+
+
+# for car in cars:
+#     print("Бренд:", car['brand'], "Модель:", car['model'], "год выпуска:", car['year'], "Максимальная скорость:", car['max_speed'], "Средняя стоимость:", car['price'])
+
+
+# for car in cars:
+#     if car['brand'] in ["Toyota", "Nissan"]:
+#         print("Найденные машины:", car['brand'], car['model'], car['max_speed'], car['year'])
+
+
+
+# for car in cars:
+#     if car['brand'] == "Toyota":
+#         print("Найденные машины:", car['brand'], car['model'], car['max_speed'], car['year'])
+
+
+
+# for i in range(len(cars)):
+#     if 310 <= cars[i]['max_speed'] <= 305 and cars[i]['brand'] != "Mersedes":
+#         print(f"Найденные машины: {cars[i]['brand']}, {cars[i]['model']}, {cars[i]['max_speed']}, {cars[i]['year']}")
+#     else:
+#         print(f"Найденные машины со скоростью менее 305 км/ч: {cars[i]['brand']}, {cars[i]['model']}, {cars[i]['max_speed']}, {cars[i]['year']}")
+
+
+
+# for i in range(len(cars)):
+#     if 240 <= cars[i]['max_speed'] <= 270 and cars[i]['brand'] != "Mersedes":
+#         print(f"Найденные машины: {cars[i]['brand']}, {cars[i]['model']}, {cars[i]['max_speed']}, {cars[i]['year']}")
+
+
+
+# for car in cars:
+#     if car['max_speed'] > 290 and car['brand'] != "Mersedes":
+#         print(car['brand'], car["model"], car["max_speed"])
+
+# пример выборки всех машинавтопарка с использованием разделителя
+# for car in cars:
+#     print(car['brand'], car['model'], car['year'], car['max_speed'], "Ориентировочная стоимость:", car['price'], sep='-')
+
+
+
+# пример выборки всех машинавтопарка с использованием разделителя
+# for i in range (len(cars)):
+#     print(cars[i]['brand'], cars[i]['model'], sep='*')
+
+
+
+# с использованием разделителей пример
+# for i in range(len(cars)):
+#     if cars[i]['max_speed'] == 240:
+#         print("Бренд:", cars[i]['brand'], "Модель:", cars[i] ['model'], "Год выпуска:", cars[i] ['year'], sep='***')
+
+# for car in cars:
+#     print(f"Бренд: {car['brand']}, Модель: {car['model']}, Год выпуска: {car['year']}, Максимальная скорость: {car['max_speed']}, Средняя стоимость: {car['price']} грн.")
+#
+# for car in cars:
+#     print(f"Бренд: {car['brand']}, Модель: {car['model']}, Год выпуска: {car['year']}")
+#
+# for car in cars:
+#     if car["max_speed"] > 240:
+#         print(f"Бренд: {car['brand']}, Модель: {car['model']}, Год выпуска: {car['year']}")
+#
+#
+# for i in range(len(cars)):
+#     if cars[i] ['max_speed'] > 200 and cars[i] ['brand'] == "BMW":
+#         print(f"Бренд: {cars[i] ['brand']}, Модель: {cars[i]['model']}")
+
+# for car in cars:
+#     if car["year"] > 2019:
+#         print(f"{car['brand']}, Модель: {car['model']}, Год выпуска: {car['year']}")
+#
+# for car in cars:
+#     print(f"{car['brand']}, Модель: {car['model']}, Год выпуска: {car['year']}")
+
+# for i in range(len(cars)):
+#     print(f"{cars[i]['brand']}, Модель: {cars[i]['model']}, Год выпуска: {cars[i]['year']}")
+
+# for i in range(len(cars)):
+#     print(f"{cars[i]['brand']}, Модель: {cars[i]['model']}, Год выпуска: {cars[i]['year']}")
+
+# for i in range(len(cars)):
+#     print(f"{cars[i]['brand']}, Модель: {cars[i]['model']}, Год выпуска: {cars[i]['year']}")
+
